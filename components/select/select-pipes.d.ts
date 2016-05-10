46236e45914d201cf0db21f732eda8e1248f4fee
filipows <3.3fullpower@gmail.com>
2016-05-10 11:54:22 +0200

    Fix for multiple selects on one page

---
+++ components/select/select-pipes.d.ts
@@ -0,0 +1,5 @@
+export declare class HightlightPipe {
+    transform(value: string, args: any[]): string;
+    private escapeRegexp(queryToEscape);
+}
+export declare function stripTags(input: string): string;
